fc0ade4eed0d683b83cc4fecc48039a345e7577c
kissdavid37 <kissdavid1017@gmail.com>
2022-02-03 18:37:58 +0100

    Update database and add ask definition

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import datetime
 from flask_cors import CORS,cross_origin
 import sqlite3 as sql
 import validators
-from sqlalchemy import Integer, Table,Column,MetaData,Boolean, create_engine
+from sqlalchemy import Integer, Table,Column,MetaData,Boolean, create_engine,String
 from sqlalchemy.ext.declarative import declarative_base
 #https://www.youtube.com/watch?v=WxGBoY5iNXY
 #https://www.youtube.com/watch?v=2VXQL3Pk0Bs
@@ -61,7 +61,7 @@ def create_question_model(tablename):
     table=Table(
         tablename,meta,
         Column('id',Integer,primary_key=True),
-        Column('user_id',Integer),
+        Column('user_name',String),
         Column('ask',Boolean,default=False),
         Column('help',Boolean,default=False),
         
@@ -140,19 +140,6 @@ def get_one_user(current_user,public_id):
     
     return jsonify({'user':user_data})
 
-# @app.route('/create',methods=['POST'])
-# #@token_required
-# def create_user():#current_user):
-#     # if not current_user.admin:
-#     #     return jsonify({'message': 'Cannot perform that function!'})
-#     data = request.get_json()
-#     hashed_password = generate_password_hash(data['password'], method='sha256')
-
-#     new_user = User(public_id=str(uuid.uuid4()), name=data['name'], password=hashed_password, admin=False)
-#     db.session.add(new_user)
-#     db.session.commit()
-    
-#     return jsonify({'message':'New user created'})
 
 @app.route('/register',methods=['POST'])
 def register():
@@ -271,19 +258,7 @@ def create_question(current_user):
         db.session.commit()
         return jsonify({'message':'Question created!'})
 
-# @app.route('/question/<question_id>',methods=['PUT'])
-# @token_required
-# def complete_todo(current_user,question_id):
-#     question=Question.query.filter_by(id=question_id,user_id=current_user.id).first()
-#     if not question:
-#         return jsonify({'message':'No question found!'})
     
-#     question.complete=True
-#     db.session.commit()
-    
-#     return jsonify({'message':'Todo item has been completed'})
-    
-  
 
 @app.route('/question/<question_id>',methods=['DELETE'])
 @token_required
@@ -305,20 +280,6 @@ def get_user_matches(current_user):
     
     return ''
 
-# @app.route('/ask/<question_text>', methods=['GET'])
-# @token_required
-# def ask(current_user,question_text):
-#     auth=request.authorization
-    
-#     if not auth or not auth.username or not auth.password:
-#         return make_response('Could not verify',401,{'WWWW-Authenticate':'Basic realm="Login required!"'})
-#     question=Question.query.filter_by(text=question_text).first()
-    
-#     if not question:
-#         return jsonify({'message':'No question found!'})
-#     question.ask=True
-#     db.session.commit()
-#     return jsonify({'message':'The question is asked'})
     
 @app.route('/ask/<question_text>', methods=['GET'])
 @token_required
@@ -331,7 +292,7 @@ def ask(current_user,question_text):
     if not question:
         return jsonify({'message':'No question found!'})
     table=sqlalchemy.Table(question_text,sqlalchemy.MetaData(),autoload_with=engine)
-    engine.execute(table.insert().values(user_id=current_user.id,ask=True,help=False))
+    engine.execute(table.insert().values(user_name=current_user.name,ask=True,help=False))
     return jsonify({'message':'The question is asked'})
     
 
